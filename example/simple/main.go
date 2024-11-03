68e41a9c33df9cc381d048f42c05a539b5036a50
Kohei Morita <moritakouhei@graffer.jp>
2024-11-03 23:20:36 +0900

    feat: update example

--- example/simple/main.go
+++ example/simple/main.go
@@ -4,27 +4,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"strconv"
 
 	"github.com/mrtc0/go-webauthn"
 )
 
-var webauthnUser = webauthn.WebAuthnUser{
+var webauthnUser = &webauthn.WebAuthnUser{
 	ID:          []byte("12345"),
 	Name:        "mrtc0",
 	DisplayName: "Kohei Morita",
 }
 
-var rp = webauthn.RP{
-	ID:     "localhost",
-	Name:   "localhost",
-	Origin: "http://localhost:8080",
-}
-
 var sessionStore = make(map[string]*webauthn.Session)
 
 func main() {
+	rpConfig := &webauthn.RPConfig{
+		ID:     "localhost",
+		Name:   "localhost",
+		Origin: "http://localhost:8080",
+	}
+
+	rp := webauthn.NewRelyingParty(rpConfig)
+	h := RPHandler{rp: *rp}
+
 	server := http.Server{
 		Addr: ":8080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +36,13 @@ func main() {
 			case "/":
 				http.ServeFile(w, r, "index.html")
 			case "/webauthn/registration/start":
-				registrationStart(w, r)
+				h.registrationStart(w, r)
 			case "/webauthn/registration/finish":
-				registrationFinish(w, r)
+				h.registrationFinish(w, r)
+			case "/webauthn/login/start":
+				h.loginStart(w, r)
+			case "/webauthn/login/finish":
+				h.loginFinish(w, r)
 			default:
 				http.NotFound(w, r)
 			}
@@ -44,14 +52,18 @@ func main() {
 	server.ListenAndServe()
 }
 
-func registrationStart(w http.ResponseWriter, r *http.Request) {
+type RPHandler struct {
+	rp webauthn.RelyingParty
+}
+
+func (h RPHandler) registrationStart(w http.ResponseWriter, r *http.Request) {
 	authenticatorSelection := webauthn.AuthenticatorSelectionCriteria{
 		// ref. https://passkeys.dev/docs/use-cases/bootstrapping/#a-note-about-user-verification
 		UserVerification:   "preferred",
 		ResidentKey:        "required",
 		RequireResidentKey: true,
 	}
-	options, session, err := webauthn.CreateOptionsForRegistrationCeremony(&webauthnUser, &rp, webauthn.WithAuthenticatorSelection(authenticatorSelection))
+	options, session, err := h.rp.CreateOptionsForRegistrationCeremony(webauthnUser, webauthn.WithAuthenticatorSelection(authenticatorSelection))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -64,7 +76,7 @@ func registrationStart(w http.ResponseWriter, r *http.Request) {
 	w.Write(json)
 }
 
-func registrationFinish(w http.ResponseWriter, r *http.Request) {
+func (h RPHandler) registrationFinish(w http.ResponseWriter, r *http.Request) {
 	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -92,13 +104,75 @@ func registrationFinish(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	record, err := webauthn.CreateCredential(&webauthnUser, sessionStore[string(webauthnUser.ID)], &response, &rp)
+	record, err := h.rp.CreateCredential(webauthnUser, sessionStore[string(webauthnUser.ID)], &response)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Printf("Credential: %#v\n", record)
+	webauthnUser.Credentials = append(webauthnUser.Credentials, *record)
+	fmt.Printf("Registration success, userID: %s, credentialID: %s\n", webauthnUser.ID, []byte(record.ID))
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("{verified: true}"))
+}
+
+func (h RPHandler) loginStart(w http.ResponseWriter, r *http.Request) {
+	sessionID := generateSessionID(32)
+	options, session, err := h.rp.CreateOptionsForAuthenticationCeremony([]byte(sessionID))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	sessionStore[sessionID] = session
+
+	w.Header().Set("Content-Type", "application/json")
+	json, _ := json.Marshal(options)
+	http.SetCookie(w, &http.Cookie{
+		Name:  "sessionID",
+		Value: sessionID,
+		Path:  "/",
+	})
+	w.Write(json)
+}
+
+func (h RPHandler) loginFinish(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("sessionID")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	sessionID := cookie.Value
+
+	var response webauthn.AuthenticationResponseJSON
+	if err := json.NewDecoder(r.Body).Decode(&response); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("request body:\n\t%#v\n", response)
+
+	webauthnUser, credential, err := h.rp.Authentication(func(credentialRawID []byte, userHandle string) (*webauthn.WebAuthnUser, *webauthn.CredentialRecord, error) {
+		return webauthnUser, &webauthnUser.Credentials[0], nil
+	}, sessionStore[sessionID], &response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("logged in: %s, %#v\n", webauthnUser.ID, credential)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{verified: true}"))
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func generateSessionID(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
